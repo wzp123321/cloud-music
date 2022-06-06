ac44b8c06141231cee44def1407c7477f25e4c8a
wanzp <wanzp@tiansu-china.com>
2022-06-06 19:26:48 +0800

    新增代码

--- cloud-music-web/vite.config.ts
+++ cloud-music-web/vite.config.ts
@@ -3,5 +3,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  // 开发选项
+  server: {
+    port: 9527,
+    host: '0.0.0.0',
+    fs: {
+      strict: false, // 解决Unrestricted file system access  vite对根目录的访问做了限制
+    },
+  },
+  plugins: [vue()],
 })
